17fdf016cb7385bb1b5c9957e1362aa5927b45c3
MarzanaBegum <marzanaBegum473@gmail.com>
2022-10-23 19:33:17 +0600

    navbar

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -108,7 +108,7 @@ const Header = ({
           </ul>
         </div>
       </div>
-      <div className="">
+      <div className="md:hidden">
         <MoboDraer
           isOpen={isOpen}
           setIsOpen={setIsOpen}
