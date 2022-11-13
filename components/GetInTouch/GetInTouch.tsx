0038b9492de2db6e3e810196bfcc37ff4ef6e250
AbdurRakib960 <rakibabdur960git@gmail.com>
2022-11-14 01:46:51 +0600

    thank-you-page connected

--- components/GetInTouch/GetInTouch.tsx
+++ components/GetInTouch/GetInTouch.tsx
@@ -2,127 +2,137 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import emailjs from "emailjs-com";
 import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 const GetInTouch = () => {
-  const [loading, setLoading] = useState(false);
-  const input =
-    "w-[100%] h-[48px] md:w-[289px] lg:w-[176.16px] 2xl:w-[269px] 4xl:w-[340px] text-[#4E5256] px-[20px] py-[12px] border border-[#DADADA] rounded-[8px] outline-[#DADADA]";
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm();
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const input =
+        "w-[100%] h-[48px] md:w-[289px] lg:w-[176.16px] 2xl:w-[269px] 4xl:w-[340px] text-[#4E5256] px-[20px] py-[12px] border border-[#DADADA] rounded-[8px] outline-[#DADADA]";
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors },
+    } = useForm();
 
-  const onSubmit = async (data: any) => {
-    setLoading(true);
-    try {
-      const formData: any = {
-        name: data.name,
-        email: data.email,
-        phone: data.phone,
-        message: data.message,
-      };
-      await emailjs
-        .send(
-          `${process.env.NEXT_PUBLIC_SERVICE_ID}`,
-          `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`,
-          formData,
-          `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`
-        )
-        .then((data) => {
-          // console.log("data",data)
-          setLoading(false);
-          toast.success("Your request has been submitted!");
-          reset()
-        });
-    } catch (error) {
-      console.log("error",error)
-      setLoading(false);
-      toast.error("Your request failed!");
-    }
-  };
-  return (
-    <div className="bg-[#FFFFFF] lg:rounded-[8px] lg:shadow-lg  w-[100%] md:w-[668px] lg:w-[429px] 2xl:w-[638px] 4xl:w-[828px] h-auto">
-      <div className="xs:py-[40px] xs:px-[40px] md:px-[40px] lg:px-[23.31px] 2xl:px-[45px] lg:py-[50px]">
-        <h2 className="text-[#31265A] font-medium text-[24px] xs:text-[30px] md:text-[30px] md:leading-[125%] mb-[10px]">
-          Get In Touch With Us
-        </h2>
-        <h3 className="font-normal xs:text-[14px] md:text-[16px] leading-[100%] mb-[40px] xs:mb-[40px] md:mb-[30px] lg:mb-[37px] text-[#4E5256]">
-          Let’s contact us if you have any questions?
-        </h3>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            {...register("name", { required: true })}
-            className="w-[100%] h-[48px] mb-[10px] text-[14px] xs:text-[16px] px-[20px] py-[12px] text-[#4E5256] font-normal border border-[#DADADA] rounded-[8px] outline-[#DADADA]"
-            style={{
-              border: errors.name && "1px solid #E51937",
-            }}
-            placeholder="Your name"
-          />
-          {errors.name && (
-            <h3 className="text-red-500 text-[12px]">Name is required</h3>
-          )}
-          <div className="flex flex-col gap-[20px] mt-[10px] xs:flex-row xs:gap-[10px] md:gap-[0px] xs:justify-between">
-            <div>
-              <input
-                {...register("email", { required: true })}
-                className={input}
-                style={{
-                  border: errors.email && "1px solid #E51937",
-                }}
-                placeholder="Email"
-              />
-              {errors.email && (
-                <h3 className="text-red-500 mt-[8px] text-[12px]">
-                  Email is required
+    const onSubmit = async (data: any) => {
+        setLoading(true);
+        try {
+            const formData: any = {
+                name: data.name,
+                email: data.email,
+                phone: data.phone,
+                message: data.message,
+            };
+            await emailjs
+                .send(
+                    `${process.env.NEXT_PUBLIC_SERVICE_ID}`,
+                    `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`,
+                    formData,
+                    `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`
+                )
+                .then((data) => {
+                    // console.log("data",data)
+                    setLoading(false);
+                    toast.success("Your request has been submitted!");
+                    reset();
+                    router.push("/thank-you/message-recieved");
+                });
+        } catch (error) {
+            console.log("error", error);
+            setLoading(false);
+            toast.error("Your request failed!");
+        }
+    };
+    return (
+        <div className="bg-[#FFFFFF] lg:rounded-[8px] lg:shadow-lg  w-[100%] md:w-[668px] lg:w-[429px] 2xl:w-[638px] 4xl:w-[828px] h-auto">
+            <div className="xs:py-[40px] xs:px-[40px] md:px-[40px] lg:px-[23.31px] 2xl:px-[45px] lg:py-[50px]">
+                <h2 className="text-[#31265A] font-medium text-[24px] xs:text-[30px] md:text-[30px] md:leading-[125%] mb-[10px]">
+                    Get In Touch With Us
+                </h2>
+                <h3 className="font-normal xs:text-[14px] md:text-[16px] leading-[100%] mb-[40px] xs:mb-[40px] md:mb-[30px] lg:mb-[37px] text-[#4E5256]">
+                    Let’s contact us if you have any questions?
                 </h3>
-              )}
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        {...register("name", { required: true })}
+                        className="w-[100%] h-[48px] mb-[10px] text-[14px] xs:text-[16px] px-[20px] py-[12px] text-[#4E5256] font-normal border border-[#DADADA] rounded-[8px] outline-[#DADADA]"
+                        style={{
+                            border: errors.name && "1px solid #E51937",
+                        }}
+                        placeholder="Your name"
+                    />
+                    {errors.name && (
+                        <h3 className="text-red-500 text-[12px]">
+                            Name is required
+                        </h3>
+                    )}
+                    <div className="flex flex-col gap-[20px] mt-[10px] xs:flex-row xs:gap-[10px] md:gap-[0px] xs:justify-between">
+                        <div>
+                            <input
+                                {...register("email", { required: true })}
+                                className={input}
+                                style={{
+                                    border: errors.email && "1px solid #E51937",
+                                }}
+                                placeholder="Email"
+                            />
+                            {errors.email && (
+                                <h3 className="text-red-500 mt-[8px] text-[12px]">
+                                    Email is required
+                                </h3>
+                            )}
+                        </div>
+                        <div>
+                            <input
+                                {...register("phone", { required: true })}
+                                className={input}
+                                style={{
+                                    border: errors.phone && "1px solid #E51937",
+                                }}
+                                placeholder="Phone"
+                            />
+                            {errors.phone && (
+                                <h3 className="text-red-500 mt-[8px] text-[12px]">
+                                    Phone is required
+                                </h3>
+                            )}
+                        </div>
+                    </div>
+                    <textarea
+                        {...register("message", { required: true })}
+                        rows={3}
+                        cols={3}
+                        placeholder="Message"
+                        style={{
+                            border: errors.message && "1px solid #E51937",
+                        }}
+                        className="w-[100%] mt-[20px] text-[#4E5256] px-[20px] py-[12px] border border-[#DADADA] rounded-[8px] outline-[#DADADA]"
+                    />
+                    {errors.message && (
+                        <h3 className="text-red-500 mt-[8px] text-[12px]">
+                            Message is required
+                        </h3>
+                    )}
+                    <button
+                        type="submit"
+                        style={{
+                            boxShadow:
+                                "0px 6px 16px -2px rgba(38, 31, 64, 0.16)",
+                        }}
+                        className="w-[100%] mt-[30px] cursor-pointer md:w-[224px] font-medium  h-[55px] bg-[#866EE1] border-[#866EE1] rounded-[72px] 2xl:text-[18px] text-[#FFFFFF] hover:bg-primary"
+                    >
+                        {loading ? "Sending..." : "Send Request"}{" "}
+                        <span className={`${loading && "hidden"} text-[20px]`}>
+                            &rarr;
+                        </span>
+                    </button>
+                </form>
             </div>
-            <div>
-              <input
-                {...register("phone", { required: true })}
-                className={input}
-                style={{
-                  border: errors.phone && "1px solid #E51937",
-                }}
-                placeholder="Phone"
-              />
-              {errors.phone && (
-                <h3 className="text-red-500 mt-[8px] text-[12px]">
-                  Phone is required
-                </h3>
-              )}
-            </div>
-          </div>
-          <textarea
-            {...register("message", { required: true })}
-            rows={3}
-            cols={3}
-            placeholder="Message"
-            style={{
-              border: errors.message && "1px solid #E51937",
-            }}
-            className="w-[100%] mt-[20px] text-[#4E5256] px-[20px] py-[12px] border border-[#DADADA] rounded-[8px] outline-[#DADADA]"
-          />
-          {errors.message && (
-            <h3 className="text-red-500 mt-[8px] text-[12px]">
-              Message is required
-            </h3>
-          )}
-          <button
-            type="submit"
-            style={{ boxShadow: "0px 6px 16px -2px rgba(38, 31, 64, 0.16)" }}
-            className="w-[100%] mt-[30px] cursor-pointer md:w-[224px] font-medium  h-[55px] bg-[#866EE1] border-[#866EE1] rounded-[72px] 2xl:text-[18px] text-[#FFFFFF] hover:bg-primary"
-          >
-            {loading ? "Sending..." : "Send Request"}{" "}
-            <span className={`${loading && "hidden"} text-[20px]`}>&rarr;</span>
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default GetInTouch;
--- components/NewsLetter/index.tsx
+++ components/NewsLetter/index.tsx
@@ -3,58 +3,61 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
 
 const NewsLetter = ({ status, message, onValidated }: any) => {
-  console.log("message",message,status)
-  const validate = Yup.object({
-    email: Yup.string().email().required("Enter a valid email!"),
-  });
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(validate),
-  });
+    const router = useRouter();
+    console.log("message", message, status);
+    const validate = Yup.object({
+        email: Yup.string().email().required("Enter a valid email!"),
+    });
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(validate),
+    });
 
-  const onSubmit = async (data: any) => {
-    try {
-      await onValidated({ EMAIL: data.email })
-      toast.success("Thank you for subscribing!");
-      reset()
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div>
-      <h2 className="font-bold text-[24px] leadign-[33px] mb-[25px] xs:text-[24px] xs:leadign-[33px] xs:mb-[25px] md:text-[36px] md:leading-[49px] md:mb-[42px] lg:text-[48px] lg:mb-[42px] lg:leading-[66px] xl:text-[48px] xl:mb-[42px] xl:leading-[66px] 2xl:text-[48px] 2xl:mb-[42px] 2xl:leading-[66px] 3xl:text-[48px] 3xl:mb-[42px] 3xl:leading-[66px] 4xl:text-[48px] 4xl:leading-[66px] 4xl:mb-[40px] text-[#FFFFFF] text-center">
-        Subscribe to Our Newsletter
-      </h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mx-auto border-[#ffffff80] mb-[50px] xs:mb-[50px] md:mb-[77px] lg:mb-[50px] 4xl:mb-[75.11px] border w-[100%] h-[58px] xs:w-[440.44px] xs:h-[58px] md:w-[637.88px] lg:w-[697px] md:h-[84px] rounded-[20px]">
-          <input
-            {...register("email")}
-            placeholder="Enter your email address"
-            className="w-[64%] xs:w-[65%] lg:w-[68%] px-[20px] text-[12px] py-[19px] xs:px-[20px] xs:text-[12px] xs:py-[19px] md:px-[28.37px] md:text-[20px] md:py-[26px] lg:px-[31px] 4xl:px-[31px] 4xl:py-[26px] 4xl:text-[20px] font-normal text-[#ffffff80] bg-transparent border-none outline-none"
-          />
-          <button
-            type="submit"
-            className="text-[16px] w-[35%] xs:w-[144.87px] xs:text-[16px] md:w-[209.81px] md:text-[20px] lg:w-[217.65px] 4xl:w-[217.65px] 4xl:text-[20px] h-[100%] float-right rounded-[20px] bg-[#F3F1FC] text-[#31265A] font-bold cursor-pointer border border-solid border-inherit hover:bg-[#866EE1] hover:text-white transition ease-in-out duration-[300ms]"
-          >
-            {status === "sending" ? "Sending..." : "Submit"}
-          </button>
+    const onSubmit = async (data: any) => {
+        try {
+            await onValidated({ EMAIL: data.email });
+            toast.success("Thank you for subscribing!");
+            reset();
+            router.push("/thank-you/signed-up");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div>
+            <h2 className="font-bold text-[24px] leadign-[33px] mb-[25px] xs:text-[24px] xs:leadign-[33px] xs:mb-[25px] md:text-[36px] md:leading-[49px] md:mb-[42px] lg:text-[48px] lg:mb-[42px] lg:leading-[66px] xl:text-[48px] xl:mb-[42px] xl:leading-[66px] 2xl:text-[48px] 2xl:mb-[42px] 2xl:leading-[66px] 3xl:text-[48px] 3xl:mb-[42px] 3xl:leading-[66px] 4xl:text-[48px] 4xl:leading-[66px] 4xl:mb-[40px] text-[#FFFFFF] text-center">
+                Subscribe to Our Newsletter
+            </h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mx-auto border-[#ffffff80] mb-[50px] xs:mb-[50px] md:mb-[77px] lg:mb-[50px] 4xl:mb-[75.11px] border w-[100%] h-[58px] xs:w-[440.44px] xs:h-[58px] md:w-[637.88px] lg:w-[697px] md:h-[84px] rounded-[20px]">
+                    <input
+                        {...register("email")}
+                        placeholder="Enter your email address"
+                        className="w-[64%] xs:w-[65%] lg:w-[68%] px-[20px] text-[12px] py-[19px] xs:px-[20px] xs:text-[12px] xs:py-[19px] md:px-[28.37px] md:text-[20px] md:py-[26px] lg:px-[31px] 4xl:px-[31px] 4xl:py-[26px] 4xl:text-[20px] font-normal text-[#ffffff80] bg-transparent border-none outline-none"
+                    />
+                    <button
+                        type="submit"
+                        className="text-[16px] w-[35%] xs:w-[144.87px] xs:text-[16px] md:w-[209.81px] md:text-[20px] lg:w-[217.65px] 4xl:w-[217.65px] 4xl:text-[20px] h-[100%] float-right rounded-[20px] bg-[#F3F1FC] text-[#31265A] font-bold cursor-pointer border border-solid border-inherit hover:bg-[#866EE1] hover:text-white transition ease-in-out duration-[300ms]"
+                    >
+                        {status === "sending" ? "Sending..." : "Submit"}
+                    </button>
 
-          {errors.email && (
-            <h3 className="text-red-500 mt-[10px] ml-[10px] text-[12px]">
-              Enter a valid email!
-            </h3>
-          )}
+                    {errors.email && (
+                        <h3 className="text-red-500 mt-[10px] ml-[10px] text-[12px]">
+                            Enter a valid email!
+                        </h3>
+                    )}
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 export default NewsLetter;
