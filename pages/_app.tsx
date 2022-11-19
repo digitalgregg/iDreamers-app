83aa70172e5d682cb3bf03dc39c0dc05d3dc3575
AbdurRakib960 <rakibabdur960git@gmail.com>
2022-11-19 20:36:54 +0600

    gooly-tag and google analytics add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Script from "next/script";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -26,6 +27,29 @@ function MyApp({ Component, pageProps }: AppProps) {
                 type="text/javascript"
                 src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=WCuDXq"
             ></Script>
+
+            <Script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=UA-193587047-3"
+            ></Script>
+
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-193587047-3');
+        `}
+            </Script>
+
+            <Script id="google-tag-manager">
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K9ZR5ZP')`}
+            </Script>
 
             <Component {...pageProps} />
         </>
